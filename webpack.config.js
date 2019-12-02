958da25952a7a692509c33ccb8cd2f802c86364d
plastikfan <plastikfan@zoho.com>
2019-12-02 18:24:42 +0000

    chore(config): setup typescript in project
    chore #18

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,55 @@
+
 const path = require('path');
 const webpack = require('webpack');
-const { getIfUtils, removeEmpty } = require('webpack-config-utils');
+const { getIfUtils } = require('webpack-config-utils');
 const nodeExternals = require('webpack-node-externals');
 
 module.exports = (env) => {
   const {
-    ifProduction,
-    ifNotTest,
-    ifTest
+    ifProduction
   } = getIfUtils(env);
 
   const mode = ifProduction('production', 'development');
   console.log('>>> Zenobia Webpack Environment mode: ' + env.mode);
 
   return {
-    mode: mode,
-    entry: removeEmpty([
-      ifNotTest('./index.js'),
-      ifTest('./tests/all-tests-entry.js')
-    ]),
-    module: {
-      rules: [{
-        test: /index.js/,
-        use: 'shebang-loader'
-      }]
+    devtool: 'source-map',
+    entry: {
+      index: './lib/index.ts'
     },
-    plugins: removeEmpty([
-      ifNotTest(new webpack.BannerPlugin({
+    target: 'node',
+    mode: mode,
+    externals: [nodeExternals()],
+    module: {
+      rules: [
+        {
+          test: /\.ts(x?)$/,
+          use: 'ts-loader'
+        },
+        {
+          test: /\.json$/,
+          use: 'json-loader'
+        }
+      ]
+    },
+    plugins: [
+      new webpack.DefinePlugin({ 'process.env.NODE_ENV': '"production"' }),
+      new webpack.BannerPlugin({
         banner: '#!/usr/bin/env node',
         raw: true
-      }))
-    ]),
-    externals: [nodeExternals()],
-    output: removeEmpty(
-      ifNotTest({
-        filename: 'zenobia-bundle.js',
-        path: path.resolve(__dirname, 'dist')
-      }),
-      ifTest({
-        filename: 'zenobia-test-bundle.js',
-        path: path.resolve(__dirname, 'dist')
-      }))
+      })
+    ],
+    resolve: {
+      extensions: ['.ts', '.js', '.json']
+    },
+    watchOptions: {
+      ignored: /node_modules/
+    },
+    output: {
+      filename: 'zenobia-bundle.js',
+      sourceMapFilename: 'zenobia-bundle.js.map',
+      path: path.join(__dirname, 'dist'),
+      libraryTarget: 'commonjs'
+    }
   };
 };
--- webpack.config.test.js
+++ webpack.config.test.js
@@ -2,17 +2,30 @@ const path = require('path');
 const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
+  devtool: 'source-map',
   mode: 'development',
-  entry: './tests/all-tests-entry.js',
+  entry: ['./tests/all-tests-entry.js', './lib'],
   externals: [nodeExternals()],
   module: {
-    rules: [{
-      test: /\.xml$/i,
-      use: 'raw-loader'
-    }]
+    rules: [
+      {
+        test: /\.xml$/i,
+        use: 'raw-loader'
+      },
+      { test: /\.ts(x?)$/, loader: 'ts-loader' },
+      { test: /\.json$/, loader: 'json-loader' }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.json']
+  },
+  watchOptions: {
+    ignored: /node_modules/
   },
   output: {
     filename: 'zenobia-test-bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    sourceMapFilename: 'zenobia-test-bundle.js.map',
+    path: path.resolve(__dirname, 'dist'),
+    libraryTarget: 'commonjs'
   }
 };
